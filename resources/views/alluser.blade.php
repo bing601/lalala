10d4061886435d4d8f68bed8c39ccdd5b939f829
bing601 <JIAneite21>
2018-12-17 20:59:19 -0500

    ass jquery

--- resources/views/alluser.blade.php
+++ resources/views/alluser.blade.php
@@ -42,5 +42,31 @@
         @endforeach
     </div>
 </div>
+<script type="text/javascript">
+    jQuery(document).ready(function() {
+        jQuery('.follow').click(function(){
+            jQuery.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+                }
+            });
+            var id = $(this).data('id');
+            console.log(id);
+            var reference= $(this);
+            jQuery.ajax({
+                type:'POST',
+                url:'/toggle',
+                data:{user_id:id},
+                success:function(data){
+                    if(jQuery.isEmptyObject(data.success.attached)){
+                        reference.find("strong").text("Follow");
+                    }else{
+                        reference.find("strong").text("UnFollow");
+                    }
+                }
+            });
+        });
+    });
+</script>
 </body>
 </html>
